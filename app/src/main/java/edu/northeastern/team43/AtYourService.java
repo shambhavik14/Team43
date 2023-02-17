116fc0bb111846fcfe48fb048c90f391a7df3382
DESKTOP-S7JEB9Q\shamb <kulkarni.sham@northeastern.edu>
2023-02-17 16:01:30 -0500

    adding initial UI

---
+++ app/src/main/java/edu/northeastern/team43/AtYourService.java
@@ -0,0 +1,16 @@
+package edu.northeastern.team43;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AtYourService extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_at_your_service);
+    }
+
+
+}
--- app/src/main/java/edu/northeastern/team43/MainActivity.java
+++ app/src/main/java/edu/northeastern/team43/MainActivity.java
@@ -2,7 +2,12 @@ package edu.northeastern.team43;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import edu.northeastern.team43.R.id;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +15,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button btn = (Button) findViewById(id.btnYourService);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this, AtYourService.class);
+                startActivity(intent);
+            }
+        });
     }
 }
