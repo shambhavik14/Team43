4dfed198c0f57a5c0eef9bffa37e1d95479b5176
amitsk <amitsk2014@gmail.com>
2023-03-04 17:45:57 -0500

    minor changes related to login

--- app/src/main/java/edu/northeastern/team43/LoginActivity.java
+++ app/src/main/java/edu/northeastern/team43/LoginActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.app.Dialog;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -15,8 +16,14 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.Iterator;
+import java.util.Map;
 
 public class LoginActivity extends AppCompatActivity {
     Button loginBttn;
@@ -28,7 +35,7 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.login);
 
 
-        databaseReference = FirebaseDatabase.getInstance().getReference().child("users");
+        databaseReference = FirebaseDatabase.getInstance().getReference().child("Users");
         loginBttn=(Button)findViewById(R.id.login);
         userNameText=(EditText)findViewById(R.id.username);
         loginBttn.setOnClickListener(new View.OnClickListener() {
@@ -41,46 +48,36 @@ public class LoginActivity extends AppCompatActivity {
 
     private void login(){
         String username = userNameText.getText().toString();
-        FirebaseUser currentUser = FirebaseAuth.getInstance().getCurrentUser();
-        if (currentUser == null){
-            FirebaseAuth.getInstance()
-                    .createUserWithEmailAndPassword(username.trim().concat("@gmail.com"),"123456")
-                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            databaseReference.push()
-                                    .setValue(new UserModel(FirebaseAuth.getInstance().getUid(),username));
-                            Intent intent=new Intent(getApplicationContext(),MainmenuActivity.class);
-                            startActivity(intent);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            e.printStackTrace();
-                        }
-                    });
-        }else{
-            FirebaseAuth.getInstance()
-                    .signInWithEmailAndPassword(username,"123456")
-                    .addOnSuccessListener(new OnSuccessListener<AuthResult>() {
-                        @Override
-                        public void onSuccess(AuthResult authResult) {
-                            Intent intent=new Intent(getApplicationContext(),MainmenuActivity.class);
-                            startActivity(intent);
-                        }
-                    }).addOnFailureListener(new OnFailureListener() {
-                        @Override
-                        public void onFailure(@NonNull Exception e) {
-                            Dialog authFailureDialog = new Dialog(LoginActivity.this);
-                            authFailureDialog.setContentView(R.layout.auth_failure);
-                            authFailureDialog.show();
-                            Button closeButton = authFailureDialog.findViewById(R.id.close_button);
-                            closeButton.setOnClickListener(v->{
-                                authFailureDialog.dismiss();
-                            });
-                        }
-                    });
-        }
+        databaseReference.orderByChild("userName").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Iterable<DataSnapshot> children = snapshot.getChildren();
+                Log.d("", children.toString());
+                Iterator<DataSnapshot> iterator = children.iterator();
+                while (iterator.hasNext()){
+
+                    Map<String,String> map = (Map<String,String>) iterator.next().getValue();
+                    Log.d("", map.entrySet().toString());
+                    if (map.containsValue(username)){
+                        Intent intent=new Intent(getApplicationContext(),MainmenuActivity.class);
+                        startActivity(intent);
+                        return;
+                    }
+                }
+                Dialog authFailureDialog = new Dialog(LoginActivity.this);
+                authFailureDialog.setContentView(R.layout.auth_failure);
+                authFailureDialog.show();
+                Button closeButton = authFailureDialog.findViewById(R.id.close_button);
+                closeButton.setOnClickListener(v->{
+                    authFailureDialog.dismiss();
+                });
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.d("", error.getMessage());
+            }
+        });
 
     }
 }
--- app/src/main/java/edu/northeastern/team43/MainActivity.java
+++ app/src/main/java/edu/northeastern/team43/MainActivity.java
@@ -2,16 +2,23 @@ package edu.northeastern.team43;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
@@ -36,23 +43,39 @@ public class MainActivity extends AppCompatActivity {
 
 
 
-             userModel = new UserModel();
-             userModel.setUserName("amit");
-             userModel.setPassword("123");
+             userModel = new UserModel(FirebaseAuth.getInstance().getUid(), "amit");
 
-             userModel1 = new UserModel();
-             userModel1.setUserName("pragya");
-             userModel1.setPassword("123");
 
-             userModel2 = new UserModel();
-             userModel2.setUserName("shambhavi");
-             userModel2.setPassword("123");
+             userModel1 = new UserModel(FirebaseAuth.getInstance().getUid(),"pragya");
+
+
+             userModel2 = new UserModel(FirebaseAuth.getInstance().getUid(),"shambhavi");
+
 
 
              userDataRef = FirebaseDatabase.getInstance().getReference().child("Users");
-             userDataRef.push().setValue(userModel);
-             userDataRef.push().setValue(userModel1);
-             userDataRef.push().setValue(userModel2);
+            Query q= userDataRef.orderByChild("userName");
+            q.addValueEventListener(new ValueEventListener() {
+                @Override
+                public void onDataChange(@NonNull DataSnapshot snapshot) {
+                    if (!snapshot.exists()){
+                        userDataRef.push().setValue(userModel);
+                        userDataRef.push().setValue(userModel1);
+                        userDataRef.push().setValue(userModel2);
+                    }
+
+                }
+
+                @Override
+                public void onCancelled(@NonNull DatabaseError error) {
+                    Log.d("", error.getMessage());
+
+                }
+            });
+
+
+
+
 
 
         covidNewsApi = (Button) findViewById(id.btnYourService);
