4976183a9430213d4c139dade506e0a43b78cc7b
Pragya <pragyaprashar490@gmail.com>
2023-04-09 16:09:05 -0400

    Created Splash Activity and designed login page activity

--- app/src/main/java/edu/northeastern/team43/project/Companion.java
+++ app/src/main/java/edu/northeastern/team43/project/Companion.java
@@ -8,6 +8,8 @@ import android.content.Intent;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -44,6 +46,13 @@ public class Companion extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+
         setContentView(R.layout.activity_companion);
         if (FirebaseAuth.getInstance().getCurrentUser()!=null){
             FirebaseAuth.getInstance().signOut();
