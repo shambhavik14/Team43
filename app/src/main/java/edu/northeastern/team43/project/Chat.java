9e66ae1feaa64e38a9f5a1d9c74a4b7670eace1e
Pragya <pragyaprashar490@gmail.com>
2023-04-11 16:13:56 -0400

    developed chat from sender to receiver

--- app/src/main/java/edu/northeastern/team43/project/Chat.java
+++ app/src/main/java/edu/northeastern/team43/project/Chat.java
@@ -5,48 +5,46 @@ import static edu.northeastern.team43.project.SearchDoctorAdapter.context;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.cardview.widget.CardView;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.annotation.SuppressLint;
-import android.content.Intent;
-import android.graphics.Color;
-import android.graphics.Typeface;
 import android.os.Bundle;
-import android.os.Message;
 import android.util.Log;
 import android.view.View;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
 
-import java.util.Date;
+import java.util.ArrayList;
 import java.util.Iterator;
 
 import edu.northeastern.team43.R;
-import edu.northeastern.team43.UserModel;
 
 public class Chat extends AppCompatActivity {
     FirebaseAuth firebaseAuth;
+    ChatAdapter messageAdapter;
     DatabaseReference databaseReference;
 //    TextView textView;
     TextView chatUser;
     CardView sendButton;
     EditText chatMessage;
+    RecyclerView recyclerView;
+    ArrayList<ChatModel> chatModelArrayList;
 
     @SuppressLint("MissingInflatedId")
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
+        recyclerView=findViewById(R.id.chatadapter);
 //        textView = findViewById(R.id.lgu);
         sendButton=findViewById(R.id.sendbttn);
         chatMessage=findViewById(R.id.chatexchanged);
@@ -98,7 +96,7 @@ public class Chat extends AppCompatActivity {
                                 .message(msg)
                                 .build();
                         databaseReference.child(key1).setValue(chatModel);
-
+                        Toast.makeText(Chat.this,"Message Successfully Sent", Toast.LENGTH_SHORT).show();
                     }
 
                     @Override
@@ -110,6 +108,34 @@ public class Chat extends AppCompatActivity {
 
             }
         });
+
+
+        chatModelArrayList=new ArrayList<>();
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("chats").orderByChild("chatId").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Iterator<DataSnapshot> iterator = snapshot.getChildren().iterator();
+                while (iterator.hasNext()){
+                    ChatModel chatModel = iterator.next().getValue(ChatModel.class);
+                    if(chatModel.getSenderEmail().equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())&&chatModel.getReceiverEmail().equalsIgnoreCase(doctorModel.getEmail())){
+                        chatModelArrayList.add(chatModel);
+                    }
+                }
+
+                setAdapter();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
 
 //        textView.setText("Welcome" + firebaseAuth.getCurrentUser().getDisplayName());
 //        databaseReference.child("patients").orderByChild("patientId").addListenerForSingleValueEvent(new ValueEventListener() {
@@ -134,5 +160,17 @@ public class Chat extends AppCompatActivity {
 //
 
 
+    }
+
+
+    private void setAdapter(){
+        Log.println(Log.DEBUG, "","this is adapter");
+        messageAdapter=new ChatAdapter(this,chatModelArrayList);
+        RecyclerView.LayoutManager linearLayoutManager=new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setAdapter(messageAdapter);
+        messageAdapter.notifyDataSetChanged();
+
+        Toast.makeText(Chat.this,"Call to recycler view", Toast.LENGTH_SHORT).show();
     }
 }
---
+++ app/src/main/java/edu/northeastern/team43/project/ChatAdapter.java
@@ -0,0 +1,108 @@
+package edu.northeastern.team43.project;
+
+import static edu.northeastern.team43.project.SearchDoctorAdapter.context;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.firebase.auth.FirebaseAuth;
+
+import java.util.ArrayList;
+
+import edu.northeastern.team43.R;
+
+public class ChatAdapter extends RecyclerView.Adapter {
+    Context contex;
+    ArrayList<ChatModel> messagesArrayList;
+    int ITEM_SEND = 1;
+    int ITEM_RECEIVE = 2;
+    FirebaseAuth firebaseAuth;
+
+    public ChatAdapter(Context contex, ArrayList<ChatModel> messagesArrayList) {
+        this.contex = contex;
+        this.messagesArrayList = messagesArrayList;
+        firebaseAuth=FirebaseAuth.getInstance();
+    }
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+
+        if(viewType==ITEM_SEND){
+            View view= LayoutInflater.from(context).inflate(R.layout.senderlayout,parent,false);
+            return new SenderViewHolder(view);
+        }
+        else{
+            View view= LayoutInflater.from(context).inflate(R.layout.receiverlayout,parent,false);
+            return new ReceiverViewHolder(view);
+        }
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+//        ChatModel chatModel=messagesArrayList.get(position);
+//        if(holder.getClass()==SenderViewHolder.class){
+//            SenderViewHolder viewHolder= (SenderViewHolder) holder;
+//            viewHolder.txtsenderMessage.setText(chatModel.getMessage());
+//        }
+//        ReceiverViewHolder viewHolder= (ReceiverViewHolder) holder;
+//        viewHolder.txtreceiverMessage.setText(chatModel.getMessage());
+
+        ChatModel chatModel=messagesArrayList.get(position);
+        if(chatModel.getSenderEmail().equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())){
+            SenderViewHolder viewHolder=(SenderViewHolder) holder;
+            viewHolder.txtsenderMessage.setText(chatModel.getMessage());
+        }
+       else if(chatModel.getReceiverEmail().equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())){
+            ReceiverViewHolder viewHolder=(ReceiverViewHolder) holder;
+            viewHolder.txtreceiverMessage.setText(chatModel.getMessage());
+        }
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return messagesArrayList.size();
+    }
+
+    @Override
+    public int getItemViewType(int position) {
+        ChatModel chatModel=messagesArrayList.get(position);
+        if(FirebaseAuth.getInstance().getCurrentUser().getEmail().equals(chatModel.getSenderEmail())){
+            return ITEM_SEND;
+        }else{
+            return ITEM_RECEIVE;
+        }
+
+    }
+
+    class SenderViewHolder extends RecyclerView.ViewHolder {
+        ImageView senderImage;
+        TextView txtsenderMessage;
+
+        public SenderViewHolder(@NonNull View itemView) {
+            super(itemView);
+            senderImage = itemView.findViewById(R.id.senderimg);
+            txtsenderMessage = itemView.findViewById(R.id.sendermessage);
+        }
+    }
+
+    class ReceiverViewHolder extends RecyclerView.ViewHolder {
+        ImageView receiverImage;
+        TextView txtreceiverMessage;
+
+        public ReceiverViewHolder(@NonNull View itemView) {
+            super(itemView);
+            receiverImage = itemView.findViewById(R.id.receiverimg);
+            txtreceiverMessage = itemView.findViewById(R.id.receivermessage);
+        }
+    }
+
+}
