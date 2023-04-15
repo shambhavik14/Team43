981af2fe718e9d339984d006fbbffe5b627cb79b
Pragya <pragyaprashar490@gmail.com>
2023-04-15 15:18:19 -0400

    splash activity created.

---
+++ app/src/main/java/edu/northeastern/team43/project/SplashScreen.java
@@ -0,0 +1,31 @@
+package edu.northeastern.team43.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.view.WindowManager;
+
+import edu.northeastern.team43.R;
+
+public class SplashScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splash_screen);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent(SplashScreen.this, Companion.class));
+                finish();
+            }
+        }, 3000);
+    }
+}
