7dd73da27be5d07a41e379f1c7a1631807cb353d
Pragya <pragyaprashar490@gmail.com>
2023-04-15 19:52:22 -0400

    List of Doctors UX/UI created

--- app/src/main/java/edu/northeastern/team43/project/SearchDoctorActivity.java
+++ app/src/main/java/edu/northeastern/team43/project/SearchDoctorActivity.java
@@ -11,7 +11,13 @@ import android.graphics.Typeface;
 import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
@@ -25,14 +31,72 @@ import edu.northeastern.team43.R;
 
 public class SearchDoctorActivity extends AppCompatActivity {
     private RecyclerView recyclerView;
+    private TextView loggedInUserName;
+    private ImageView loggedInUserImage;
     private SearchDoctorAdapter adapter;
     private ArrayList<DoctorModel> doctorNamesList;
+    private FirebaseAuth firebaseAuth;
     DatabaseReference databaseReference;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.activity_search_doctor);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getSupportActionBar().hide();
+        firebaseAuth=FirebaseAuth.getInstance();
+
         recyclerView=findViewById(R.id.recyclerViewdoctor);
+        loggedInUserName=findViewById(R.id.loginUser);
+        loggedInUserImage=findViewById(R.id.loginuserimg);
+
+
+        databaseReference= FirebaseDatabase.getInstance().getReference();
+        databaseReference.child("doctors").orderByChild("doctorId").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Iterator<DataSnapshot> iterator = snapshot.getChildren().iterator();
+                while (iterator.hasNext()){
+                    DoctorModel doctorModel = iterator.next().getValue(DoctorModel.class);
+                    if (doctorModel.getEmail().equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())){
+                        loggedInUserName.setText(doctorModel.getName());
+                        Glide.with(getApplicationContext()).load(doctorModel.getProfilePicture()).circleCrop().into(loggedInUserImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+        databaseReference.child("patients").orderByChild("patientId").addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                Iterator<DataSnapshot> iterator=snapshot.getChildren().iterator();
+                while(iterator.hasNext()){
+                    PatientModel patientModel=iterator.next().getValue(PatientModel.class);
+                    if(patientModel.getEmail().equalsIgnoreCase(firebaseAuth.getCurrentUser().getEmail())){
+                        loggedInUserName.setText(patientModel.getName());
+                        Glide.with(getApplicationContext()).load(patientModel.getProfilePicture()).circleCrop().into(loggedInUserImage);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+
+
+
 
         doctorNamesList=new ArrayList<>();
 
